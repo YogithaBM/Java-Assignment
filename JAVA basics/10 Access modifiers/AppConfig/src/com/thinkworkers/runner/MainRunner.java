8111e2cc8163c7181d77f8576725dd67dc4011d6
Yogitha.B.M <yogithabm75@gmail.com>
2026-03-11 19:08:42 +0530

    added new file

--- JAVA basics/10 Access modifiers/AppConfig/src/com/thinkworkers/runner/MainRunner.java
+++ JAVA basics/10 Access modifiers/AppConfig/src/com/thinkworkers/runner/MainRunner.java
@@ -4,6 +4,7 @@ import com.thinkworkers.appconfig.AppConfig;
 
 public class MainRunner {
     public static void main(String[] args) {
+
         AppConfig appConfig = new AppConfig("Swiggy", 3.5f,
                 "https://www.swiggy.com/", "Swiggy Limited");
         System.out.println("App Name : " + appConfig.getAppName());
---
+++ School/src/com/thinkworks/school/School.java
@@ -0,0 +1,8 @@
+package com.thinkworks.school;
+
+public class School {
+
+   public School(){
+        System.out.println("yello");
+    }
+}
